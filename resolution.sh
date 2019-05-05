c69698eca75267e32b36aa54a295f0a22a28352f
Pankaj <pankaj.sanam@gmail.com>
2019-05-05 13:08:01 +0530

    Bash script

---
+++ resolution.sh
@@ -0,0 +1,5 @@
+cvt 1600 900 60
+xrandr --newmode "1600x900_60.00" 118.25 1600 1696 1856 2112 900 903 908 934 -hsync +vsync
+xrandr --listmonitors
+xrandr --addmode LVDS-1-1 1600x900_60.00
+xrandr -s 1600x900
